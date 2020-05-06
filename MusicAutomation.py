9aaa9181fc3275721934f47c6659a6f2a512bdd6
pant377 <49340326+pant377@users.noreply.github.com>
2020-05-06 23:57:45 +0300

    Add files via upload

---
+++ MusicAutomation.py
@@ -0,0 +1,60 @@
+import os
+import spotipy
+import requests
+import json
+import spotipy.util as util
+from secret import *
+import google_auth_oauthlib.flow
+import googleapiclient.discovery
+import googleapiclient.errors
+import youtube_dl
+scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
+
+def addtoplaylist(song,artist):
+    scope = 'playlist-modify-public'
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    tracks = []
+    track = urifinder(song,artist)
+    tracks.append(track)
+    if token:
+        try:
+            sp = spotipy.Spotify(auth=token)
+            sp.user_playlist_add_tracks(username,"5g5bvBT483nKC4NKR3wG2m",tracks)
+            print("Done..")
+        except:
+            print("Not Done Sorry ..")    
+
+def urifinder(song,artist):
+    scope = 'playlist-read-collaborative'
+    token = util.prompt_for_user_token(username, scope, client_id, client_secret, redirect_uri)
+    query = "https://api.spotify.com/v1/search?query=track%3A{}+artist%3A{}&type=track&offset=0&limit=20".format(song,artist)
+    response = requests.get(
+            query,
+            headers={
+                "Content-Type": "application/json",
+                "Authorization": "Bearer {}".format(token)
+            })
+    response_json = response.json()
+    songs = response_json["tracks"]["items"]
+    uri = songs[0]["uri"]
+    return uri
+
+def googlecrs():
+    os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
+    api_service_name = "youtube"
+    api_version = "v3"
+    client_secrets_file = "C:/Users/pantelis/Desktop/WebscrapingProject/secret.json"
+    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(client_secrets_file, scopes)
+    credentials = flow.run_console()
+    youtube = googleapiclient.discovery.build(api_service_name, api_version, credentials=credentials)
+    thename(youtube) 
+
+def thename(youtube):
+    request = youtube.playlistItems().list(
+        part="snippet,contentDetails",
+        playlistId="PLf1rT1a14dFyd59AMexc6DcEAkgincO3U"
+    )
+    response = request.execute()
+    print(response)
+
+googlecrs()
---
+++ secret.py
@@ -0,0 +1,4 @@
+client_id = "04c2442383374bd2b944ba08f5db9bd9"       
+client_secret = "7fb14484578d415bb0d21d8e230372dc"
+redirect_uri = "http://127.0.0.1:9090/"
+username = "31hcn7i33lefrofss6iqxpqgchue"
